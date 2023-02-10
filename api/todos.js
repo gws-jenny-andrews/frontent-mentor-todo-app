562d4ea512a4abe1b5ed3b2a04a8d08b32c0a6f2
gws-jenny-andrews <jenny.andrews@grace-web.solutions>
2023-02-10 19:43:29 +0000

    Initial Commit

---
+++ api/todos.js
@@ -0,0 +1,6 @@
+export default async (req,res) => {
+    res.status(200).json({
+        name:"Jenny",
+        location:"UK"
+    })
+}
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,140 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { computed, onMounted, ref } from 'vue';
+import draggable from 'vuedraggable'
+
+interface IListItem {
+  name: string,
+  order: number
+}
+
+  const message = [
+  "vue.draggable",
+  "draggable",
+  "component",
+  "for",
+  "vue.js 2.0",
+  "based",
+  "on",
+  "Sortablejs"
+];
+let order = message.length;
+
+const list = ref<IListItem[]>([])
+const isDragging = ref<boolean>(false)
+
+onMounted(() => {
+  list.value = message.map((name : string, index: number) => {
+    return { name, order: index};
+  })
+})
+
+
+const sort = () => {
+  list.value = list.value.sort((a, b) => a.order - b.order);
+}
+
+const dragOptions = computed(() => {
+  return{
+    animation: 0,
+    group: "description",
+    disabled: false,
+    ghostClass: "ghost"
+  }
+})
+
+const updateList = () => {
+  isDragging.value = false
+  list.value = list.value.map((item : IListItem, index: number) => {
+    return { name: item.name, order: index };
+  })
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="row">
+    <div class="col-2">
+      <button class="btn btn-secondary button" @click="sort">
+        To original order
+      </button>
+    </div>
+
+    <div class="col-6">
+      <h3>Transition</h3>
+      <draggable
+        class="list-group"
+        item-key="order"
+        tag="ul"
+        :component-data="{name: 'flip-list', type: 'transition' }"
+        v-model="list"
+        v-bind="dragOptions"
+        @start="isDragging = true"
+        @end="updateList"
+      >
+        <template #item="{ element }">
+          <li class="list-group-item">
+            {{ element.name }}
+          </li>
+        </template>
+      </draggable>
+    </div>
+
+   
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div>
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.button {
+  margin-top: 35px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.flip-list-move {
+  transition: transform 0.5s;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    -ms-flex-direction: column;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    flex-direction: column;
+    padding-left: 0;
+    margin-bottom: 0;
+}
+
+.list-group {
+    min-height: 20px;
+}
+
+
+.list-group-item:first-child {
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+}
+
+.list-group-item {
+    position: relative;
+    display: block;
+    padding: 0.75rem 1.25rem;
+    margin-bottom: -1px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,.125);
+}
+
+.list-group-item {
+    cursor: move;
+}
+.list-group-item i {
+  cursor: pointer;
 }
 </style>
